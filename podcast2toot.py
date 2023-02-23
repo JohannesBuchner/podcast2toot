9b5615ae1442810b3d63f1da89baadba2edb4e57
Johannes Buchner <johannes.buchner.acad@gmx.com>
2023-02-23 11:21:15 +0100

    make prettier toots with links to the mp3; tags cannot have -

--- podcast2toot.py
+++ podcast2toot.py
@@ -1,11 +1,13 @@
-"""rss2toot.py
+"""podcast2toot.py
 
 To run this:
-1) create a mastodon account bot
-2) 
+1) create a mastodon account, for example on botsin.space
+2) in profile, tick "This is a bot account"
+3) in development, click "new application", entering rss2toot, and give write:statuses rights
+4) click on the application, copy the "Your access token"
+5) modify the feeds.json file to give the feeds you want to follow
+6) run MASTODON_TOKEN=youraccesstokengoeshere MASTODON_URL=botsin.space python3 podcast2toot.py
 
-MASTODON_TOKEN=
-MASTODON_URL=
 """
 
 # based on https://shkspr.mobi/blog/2018/08/easy-guide-to-building-mastodon-bots/
@@ -14,14 +16,16 @@ MASTODON_URL=
 import time
 import json
 import os
-import feedparser
-from mastodon import Mastodon
+import sys
 import hashlib
-
+import feedparser
 from io import StringIO
 from html.parser import HTMLParser
 
+from mastodon import Mastodon
+
 class MLStripper(HTMLParser):
+    """Helper for removing remaining markup tags."""
     def __init__(self):
         super().__init__()
         self.reset()
@@ -34,59 +38,56 @@ class MLStripper(HTMLParser):
         return self.text.getvalue()
 
 def strip_tags(html):
+    """Remove remaining HTML tags and return text only."""
     s = MLStripper()
     s.feed(html)
     return s.get_data()
 
 
 def build_toot(entry):
-    ''' Take the entry dict and build a toot
-    '''
-    toot_str = ''
+    '''Create the toot text with tags and link, not exceeding 500 characters.'''
+    toot_str = '%(title)s ' % entry
 
-    toot_str += f"{entry['title']} | "
-    toot_str += f"{entry['text']}\n"
+    if entry.get('author') is not None:
+        toot_str += ' by %(author)s ' % entry
 
-    if entry.get('author'):
-        toot_str += f"Author: {entry['author']}\n"
+    toot_str += ' '.join('#' + tag.replace(' ', '') for tag in entry.get('tags', [])) + " "
+    
+    # links are counted as 23 characters long at most
+    len_current = len(toot_str) + max(23, len(entry['link']))
+    # how much do we have left?
+    len_remaining = max(0, 500 - len_current)
+    if len_remaining >= len(entry['text']):
+        toot_str += entry['text']
+    else:
+        toot_str += entry['text'][:len_remaining - 3] + "…"
+    toot_str += "\n" + entry['link']
 
-    toot_str += f"\n{entry['link']}\n"
+    return toot_str
 
-    # Tags to hashtags
-    if len(entry['tags']) > 0:
-        for tag in entry['tags']:
-            toot_str += f'#{tag.replace(" ", "")} '
+def send_toot(en, session, visibility, dry_run, language=None):
+    """Turn the dict into toot text and send the toot"""
 
-    return toot_str.strip()
-
-def send_toot(en, session, mastodon_url, token, visibility, dry_run, language=None):
-    ''' Turn the dict into toot text
-        and send the toot
-    '''
-
-    # Turn the dict into a toot
     toot_txt = build_toot(en)
 
-    if dry_run:
-        print("------")
-        #print(data['status'])
-        print(visibility, language, "::", toot_txt)
-        print("------")
-        return True
-
-    return status_post(
-        toot_txt, sensitive=False,
-        visibility=visibility, language=language
-    )
+    print("------")
+    print(visibility, language, "::")
+    print(toot_txt)
+    print("------")
+    if not dry_run:
+        return session.status_post(
+            toot_txt[:500], sensitive=False,
+            visibility=visibility, language=language
+        )
 
 
 def process_feed(feed_url, cache_file):
-    ''' Process the RSS feed and generate a toot for any entry we haven't yet seen
-    '''
+    """Get each new feed entry"""
     # Load the feed, from cache if possible
     print("checking feed ...", feed_url)
     try:
         d = json.load(open(cache_file))
+        print("   loaded from cache:", cache_file)
     except (IOError, json.decoder.JSONDecodeError):
         d = feedparser.parse(feed_url)
         with open(cache_file, 'w') as fout:
@@ -97,6 +98,9 @@ def process_feed(feed_url, cache_file):
         en = {}
         en['title'] = entry['title']
         en['link'] = entry['link']
+        for link in entry.get('links', []):
+            if link['type'].startswith('audio') or link['type'].startswith('video'):
+                en['link'] = link['href']
         en['author'] = False       
         en['tags'] = [x['term'] for x in entry.get('tags', [])]
         en['author'] = entry.get('author')
@@ -118,14 +122,14 @@ def main():
     # Mastodon config
     if 'MASTODON_TOKEN' not in os.environ:
         sys.stderr.write(__doc__)
+        sys.exit(1)
     mastodon_url = os.environ['MASTODON_URL']
     token = os.environ['MASTODON_TOKEN']
     visibility = os.getenv('MASTODON_VISIBILITY', 'public')
 
-    mastodon = Mastodon(access_token=token, api_base_url=mastodon_url)
+    session = Mastodon(access_token=token, api_base_url=mastodon_url)
     # We want to be able to use keep-alives if we're posting multiple things
     # so set up a connection pool
-    session = requests.session()
 
     # Iterate over the feeds in the config file
     for feed in feeds:
@@ -145,7 +149,7 @@ def main():
                 continue
 
             # Send the toot
-            if send_toot(en, session, mastodon_url, token, visibility, dry_run, language=feed.get('lang')):
+            if send_toot(en, session, visibility, dry_run, language=feed.get('lang')):
                 # If that worked, write hash to disk to prevent re-sending
                 with open(feed_hash_file, 'w') as fhash:
                     fhash.write(en['link'])
